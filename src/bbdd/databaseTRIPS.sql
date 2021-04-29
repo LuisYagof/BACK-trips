fdbdc165fffc94134cf8173cc7f4ecb48cfcc994
LuisYagof <luisyagofdez@gmail.com>
2021-04-29 18:51:55 +0200

    FEAT: database structure; linkedin scraping example

---
+++ src/bbdd/databaseTRIPS.sql
@@ -0,0 +1,137 @@
+create database appcourses;
+
+USE appcourses;
+
+CREATE TABLE estudiantes(
+id INT PRIMARY KEY AUTO_INCREMENT,
+nombre VARCHAR(50) NOT NULL,
+apellido VARCHAR(50) NOT NULL,
+email VARCHAR(255) NOT NULL UNIQUE,
+pass VARCHAR(50) NOT NULL,
+secreto VARCHAR(50) NOT NULL
+);
+
+CREATE TABLE docentes(
+id INT PRIMARY KEY AUTO_INCREMENT,
+nombre VARCHAR(50) NOT NULL,
+email VARCHAR(255) NOT NULL UNIQUE,
+pass VARCHAR(50)NOT NULL,
+secreto VARCHAR(50) NOT NULL,
+descripcion VARCHAR(255),
+enlace VARCHAR(255),
+foto VARCHAR(200)
+);
+
+CREATE TABLE categorias(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+descripcion VARCHAR(50)
+);
+
+CREATE TABLE cursos(
+id INT PRIMARY KEY AUTO_INCREMENT,
+nombre VARCHAR(255) NOT NULL,
+descripcion VARCHAR(255),
+enlace VARCHAR(255),
+docente INT,
+precio INT,
+duracion INT,
+idioma INT,
+categoria INT,
+bolsaEmpleo BOOLEAN,
+certificado BOOLEAN,
+media INT,
+FOREIGN KEY (docente) REFERENCES docentes(id),
+FOREIGN KEY (categoria) REFERENCES categorias(id)
+);
+
+CREATE TABLE favoritos(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+curso INT,
+estudiante INT,
+FOREIGN KEY (estudiante) REFERENCES estudiantes(id),
+FOREIGN KEY (curso) REFERENCES cursos(id)
+);
+
+CREATE TABLE profesiones(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+descripcion VARCHAR(255)
+);
+
+CREATE TABLE profesionesCursos(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+curso INT,
+profesion INT,
+FOREIGN KEY (curso) REFERENCES cursos(id),
+FOREIGN KEY (profesion) REFERENCES profesiones(id)
+);
+
+CREATE TABLE keywords(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+descripcion VARCHAR(20)
+);
+
+CREATE TABLE keywordsCursos(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+curso INT,
+keywords INT,
+FOREIGN KEY (curso) REFERENCES cursos(id),
+FOREIGN KEY (keywords) REFERENCES keywords(id)
+);
+
+-- CREATE TABLE links(
+-- id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+-- enlace VARCHAR(255),
+-- docente INT,
+-- FOREIGN KEY (docente) REFERENCES docentes(id)
+-- );
+
+CREATE TABLE reviews(
+id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+estudiante INT,
+curso INT,
+descripcion VARCHAR(255),
+valoracion INT,
+FOREIGN KEY (estudiante) REFERENCES estudiantes(id),
+FOREIGN KEY (curso) REFERENCES cursos(id)
+);
+
+INSERT INTO categorias(descripcion)
+VALUES ("Desarrollo Web");
+INSERT INTO categorias(descripcion)
+VALUES ("FrontEnd");
+INSERT INTO categorias(descripcion)
+VALUES ("BackEnd");
+INSERT INTO categorias(descripcion)
+VALUES ("Marketing Digital");
+INSERT INTO categorias(descripcion)
+VALUES ("UX/UI");
+INSERT INTO categorias(descripcion)
+VALUES ("Data Science");
+
+INSERT INTO docentes(nombre, email, pass, secreto, descripcion, enlace) 
+VALUES ("Fernando Alfaro", "alfarogr@hotmail.com", "password1", "qwerty123+", "Soy un experto en queries", "linkedin.com/alfarogr");
+INSERT INTO docentes(nombre, email, pass, secreto, descripcion, enlace) 
+VALUES ("Roberto Borroso", "rborroso@hotmail.com", "password1", "qwerty123+", "Soy diseñador y front-end developer", "linkedin.com/roborroso");
+INSERT INTO docentes(nombre, email, pass, secreto, descripcion, enlace) 
+VALUES ("Jose Luis", "zellfhirot@hotmail.com", "password1", "qwerty123+", "Soy la máquina de los endpoints", "linkedin.com/zellfhirot");
+INSERT INTO docentes(nombre, email, pass, secreto, descripcion, enlace) 
+VALUES ("Luis Fernñandez", "luisyagofdez@gmail.com", "password1", "qwerty123+", "React Nativo y bro de todo el mundo", "linkedin.com/luisyagofdez");
+
+INSERT INTO cursos(nombre,descripcion, enlace, docente, precio, duracion, idioma, categoria, bolsaEmpleo, certificado) 
+VALUES ("React Native: Aplicaciones nativas para IOS y Android", "Aprende React Native desde cero. Usa React Native para construir aplicaciones iOS y Android y profundiza en React Native", "linkcurso1.com", 1, 150, 13, 0, 1, true, false 
+);
+INSERT INTO cursos(nombre,descripcion, enlace, docente, precio, duracion, idioma, categoria, bolsaEmpleo, certificado) 
+VALUES ("JavaScript de cero a experto", "Aprende el lenguaje de programación web más popular paso a paso Con Proyectos, inc. Electron React MongoDB Node Express","linkcurso2.com", 2, 176, 16, 0, 2, false, true
+);
+INSERT INTO cursos(nombre,descripcion, enlace, docente, precio, duracion, idioma, categoria, bolsaEmpleo, certificado) 
+VALUES ("Node y APIRest", "Rest, despliegues, Heroku, Mongo, Git, GitHub, Sockets, archivos, JWT y mucho más para ser un experto en Node", "linkcurso3.com", 3, 134, 18, 0, 1, true, true
+);
+INSERT INTO cursos(nombre,descripcion, enlace, docente, precio, duracion, idioma, categoria, bolsaEmpleo, certificado) 
+VALUES ("Administrador de Salesforce", "Comienza tu Carrera Profesional como Administrador de Salesforce", "linkcurso4.com", 4, 180, 18, 0, 4, true, true
+);
+INSERT INTO cursos(nombre,descripcion, enlace, docente, precio, duracion, idioma, categoria, bolsaEmpleo, certificado) 
+VALUES ("Experto en Sketch", "Aprende a usar la herramienta más popular y profesional para diseño de interfaz", "linkcurso5.com", 5, 198, 12, 0, 5, false, false
+);
+INSERT INTO cursos(nombre,descripcion, enlace, docente, precio, duracion, idioma, categoria, bolsaEmpleo, certificado) 
+VALUES ("Python + SQLite", "Aprende a programar con clases y objetos, a usar ficheros y bases de datos SQLite, interfaces gráficas y más con Python!", "linkcurso6.com", 6, 100, 14, 0, 6, false, false
+);
---
+++ src/config/linkedin.js
@@ -0,0 +1,52 @@
+const axios = require('axios')
+const cheerio = require('cheerio')
+
+async function linkedin(kw) {
+  const urlLinkedin = `https://www.linkedin.com/jobs/search?keywords=${kw}&location=Espa%C3%B1a&geoId=105646813&trk=public_jobs_jobs-search-bar_search-submit&f_TPR=r86400&position=1&pageNum=0`
+  const htmlPage = await axios(urlLinkedin)
+  const $ = cheerio.load(htmlPage.data);
+
+  let numOf = []
+
+  $('span.results-context-header__job-count').each(function () {
+    numOf.push($(this).text().trim())
+  });
+
+  let num = Number(numOf)
+
+  console.log(num);
+  return Number(num)
+
+}
+
+linkedin("javascript");
+
+module.exports = {
+  linkedin
+}
+
+
+// server.get('/linkedin/:kw', async (req, res) => {
+//   try {
+//       let linkedinRES = await linkedin(req.params.kw)
+//       if (linkedinRES) {
+//           res.status(200).json({
+//               status: 200,
+//               ok: true,
+//               data: linkedinRES
+//           })
+//       } else {
+//           res.status(400).json({
+//               status: 200,
+//               ok: false,
+//               data: "MAL"
+//           })
+//       }
+//   } catch {
+//       res.status(500).json({
+//           status: 200,
+//           ok: false,
+//           data: "FATAL"
+//       })
+//   }
+// })
