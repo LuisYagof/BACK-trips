9552faf90c4aa00e7d0774d828d8efa02931c676
fAlfaro21 <falfarogr@hotmail.com>
2021-05-11 18:38:31 +0200

    Modificación de SQLqueries.js y endpoints.js

--- src/routes/endpoints.js
+++ src/routes/endpoints.js
@@ -10,7 +10,7 @@ const { createToken, hash, randomString, decodeToken, emailIsValid,
     const {linkedin} = require('../config/linkedin')
 
 const { newStudent, newTeacher, logUser, logout, recoverAccount, recoverPass,
-    newPass, searchAll, keywords, newReview, showFavs, newFav, deleteFav } = require("../queries/SQLqueries")
+    newPass, searchAll, keywords, newReview, showFavs, newFav, deleteFav, newCourse } = require("../queries/SQLqueries")
 
 // -------------------------------SERVIDOR Y PUERTOS
 
@@ -127,15 +127,15 @@ server.post('/logUser/:rol', async (req, res) => {
 
 // ------------------------------------------------------------------LOGOUT
 
-server.put('/logout/:rol', async (req, res) => {
+server.put('/logout', async (req, res) => {
     try {
         let newSecret = randomString()
         let token = req.headers.authorization.split(" ")[1]
         const PAYLOAD = decodeToken(token)
-        const SQLresponse = await logout(PAYLOAD, newSecret, req.params.rol)
+        const SQLresponse = await logout(PAYLOAD, newSecret, PAYLOAD.rol)
         res.status(200).json({
             status: 200,
-            ok: false,
+            ok: true,
             data: SQLresponse,
             msg: "Deslogado correctamente",
             url: '/'
@@ -292,7 +292,6 @@ server.get('/searchAll', async (req, res) => {
 server.get('/keywords/:curso', async (req, res) => {
     try {
         const SQLresponse = await keywords(req.params.curso)
-        console.log("sqlRES", SQLresponse);
         if (SQLresponse[0]) {
             const APIresponse = axios({
             method: 'get',
@@ -484,6 +483,37 @@ server.delete('/deleteFav/:curso', async (req, res) => {
                 ok: false,
                 data: SQLresponse,
                 msg: "Imposible borrar"
+            })
+        }
+    } catch (err) {
+        if (err.errno)
+            res.status(500).json({
+                status: 500,
+                ok: false,
+                data: err.sqlMessage,
+                msg: "Error en base de datos"
+            })
+    }
+})
+
+// ----------------------------------------------------------------NEW COURSE
+
+server.post('/newCourse', async (req, res) => {
+    try {
+        let token = req.headers.authorization.split(" ")[1]
+        const PAYLOAD = decodeToken(token)
+        const SQLresponse = await newCourse(req.body, PAYLOAD.id)
+        if (SQLresponse.affectedRows > 0) {
+            res.status(200).json({
+                status: 200,
+                ok: true,
+                msg: "Curso guardado correctamente",
+            })
+        } else {
+            res.status(400).json({
+                status: 400,
+                ok: false,
+                msg: "El curso ya existe",
             })
         }
     } catch (err) {
