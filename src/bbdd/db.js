cac5f1cf660d0a244cab0f79f40dc097e0911810
LuisYagof <luisyagofdez@gmail.com>
2021-05-14 20:13:50 +0200

    REFACTOR: change db configuration to interact w/ AWS database

--- src/bbdd/db.js
+++ src/bbdd/db.js
@@ -1,9 +1,16 @@
 const mysql = require('mysql');
 
+// const DB = mysql.createConnection({
+//   host     : 'localhost',
+//   user     : 'root',
+//   password : 'root',
+//   database : 'coursenine'
+// });
+
 const DB = mysql.createConnection({
-  host     : 'localhost',
-  user     : 'root',
-  password : 'root',
+  host     : 'database-1.cgms0uqi3vl9.eu-west-1.rds.amazonaws.com',
+  user     : 'admin',
+  password : 'Trip2021',
   database : 'coursenine'
 });
 
