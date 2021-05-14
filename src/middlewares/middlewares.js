1643bcbfe1ddcba16b0c50482c58c3788ee3f211
LuisYagof <luisyagofdez@gmail.com>
2021-05-14 13:51:28 +0200

    FIX: new verify method in nodemailing (middlewares)

--- src/middlewares/middlewares.js
+++ src/middlewares/middlewares.js
@@ -66,6 +66,14 @@ function mailPassword(mail, token) {
 		</table>`
 	};
 
+	transporter.verify(function (error, success) {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log("Server is ready to take our messages");
+		}
+	});
+
 	transporter.sendMail(mailOptions, function (error, info) {
 		if (error) {
 			return error;
